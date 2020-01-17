59b2f7fe7c40647e45cbc34631df1521943593c4
Oleksandr Velychko <49977771+VelychkoOleksandr@users.noreply.github.com>
2020-01-17 08:31:19 +0200

    Added pokedex

---
+++ pokedex/precache-manifest.1f7c0610bc5b04e1f44553eeb7befa92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43beded4f43373d066a2cdd2ebe6742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06de00b1cc82898ba38f",
+    "url": "/static/css/main.fd7d8921.chunk.css"
+  },
+  {
+    "revision": "828f1bafdded8cb6eb09",
+    "url": "/static/js/2.2d352e33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2d352e33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06de00b1cc82898ba38f",
+    "url": "/static/js/main.e6cc6d3d.chunk.js"
+  },
+  {
+    "revision": "ddb553b6c06cf6b1b1c0",
+    "url": "/static/js/runtime-main.11e6aad1.js"
+  }
+]);
